57d200072ed6d76086f04f0102e19b2486f12c6e
iravishah <ravi.shah@contentstack.com>
2022-07-23 23:53:17 +0530

    added debug logs

--- src/apis/base.js
+++ src/apis/base.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+console.log('process.env.NODE_ENV ', process.env.NODE_ENV);
+
 const baseURL = process.env.NODE_ENV === 'production' ? 'https://bhagidari.herokuapp.com' : 'http://localhost:4001';
 
 export default axios.create({
